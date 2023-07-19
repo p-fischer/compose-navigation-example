5922849d33354ea2212f57bd83fc7312af1c3441
Peter Fischer <p.fischer@anfe.ma>
2023-07-19 13:24:59 +0200

    Add bottom bar navigation
    There are 4 screens:
    - Home and More are reachable via the navigation bar
    - Home Detail and More Detail are navigable from the Home screen

--- app/src/main/java/com/example/navigationexample/MainScreen.kt
+++ app/src/main/java/com/example/navigationexample/MainScreen.kt
@@ -1,36 +1,113 @@
 package com.example.navigationexample
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.navigationexample.ui.theme.NavigationExampleTheme
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+sealed class Screen(val route: String, val name: String) {
+    object Home : Screen("home", "Home")
+    object HomeDetail : Screen("home_detail", "")
+    object More : Screen("more", "More")
+    object MoreDetail : Screen("more_detail", "")
+}
+
+val bottomBarItems = listOf(Screen.Home, Screen.More)
 
 @Composable
 fun MainScreen() {
-    Surface(
+    val navController = rememberNavController()
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting("Android")
-    }
-}
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                bottomBarItems.forEach { screen ->
+                    NavigationBarItem(
+                        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                        label = { Text(screen.name) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                // Pop up to the start destination of the graph to
+                                // avoid building up a large stack of destinations
+                                // on the back stack as users select items
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                // Avoid multiple copies of the same destination when
+                                // reselecting the same item
+                                launchSingleTop = true
+                                // Restore state when reselecting a previously selected item
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigationExampleTheme {
-        Greeting("Android")
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable(Screen.Home.route) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Button(onClick = { navController.navigate(Screen.HomeDetail.route) }) {
+                        Text("Go to Home Detail")
+                    }
+                }
+            }
+            composable(Screen.HomeDetail.route) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Text("Home Detail")
+                }
+            }
+            composable(Screen.More.route) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Button(onClick = { navController.navigate(Screen.MoreDetail.route) }) {
+                        Text("Go to More Detail")
+                    }
+                }
+            }
+            composable(Screen.MoreDetail.route) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Text("More Detail")
+                }
+            }
+        }
     }
 }
